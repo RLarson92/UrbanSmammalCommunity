0b99c4b205b12a4de2c28933a534b7adc4d57652
Rachel Larson, PhD <126201983+RLarson92@users.noreply.github.com>
2024-06-06 16:39:32 -0500

    Update Connectivity.R
    Changed how the results were saved to facilitate "stitching together" this dataset with others in the analysis

--- landscapes/Connectivity.R
+++ landscapes/Connectivity.R
@@ -3,7 +3,7 @@
 #                                 Code for calculating contagion index values                             #
 #                                          using `landscapemetrics`                                       #
 #                                                                                                         #
-#                                         Last edited: 25 Jan 2024                                        #
+#                                         Last edited: 6 Jun 2024                                        #
 #                                                                                                         #
 ###########################################################################################################
 # You'll need to load the 'raster' and 'landscapemetrics' packages to perform this analysis
@@ -26,14 +26,18 @@ for(i in 1:length(allRasters)){
 }
 # a couple of the trap sites contain only 1 land cover class, and will cause a warning
 # from landscapemetrics. When inspected visually in ArcMap or the like, you'll see their
-# contagion value should be 0 (i.e., low interspersion and high "clumping"). I hand-edited
-# the '0's into my covariate datafile
+# contagion value should be 0 (i.e., low interspersion and high "clumping"). You'll have
+# to tell R to replace the resulting 'NA's with '0'
 
 # writing the results into that blank list and adding the site name
 results_contag <- rlist::list.rbind(resultsList_contag)
-results_contag$site <- rasterList
+results_contag$site <- c(1,11,111,117,12,120,122,123,124,135,136,140,142,143,147,148,161,162,17,
+                         205,214,216,221,222,223,225,227,229,23,245,246,250,255,256,260,264,269,274,288,289,295,
+                         8,83,88,90)
+# subsetting just the contagion values
+results_contag <- results_contag[,6:7]
+results_contag$value[is.na(results_contag$value)] <- 0
+colnames(results_contag) <- c("contag","site")
 
-# writing the results to a .csv. I was lazy and just copied the values from this .csv
-# into the bigger .csv I have that has all the plot covariates, rather than creating
-# an R script to stitch them together
+# writing the results to a .csv
 write.csv(results_contag, "./data/contag.csv", row.names = FALSE)
